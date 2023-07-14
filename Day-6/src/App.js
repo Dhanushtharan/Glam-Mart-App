1fb0a851486b42e1c0e285637ebb840a827cda70
Dhanush Tharan M <106361969+Dhanushtharan@users.noreply.github.com>
2023-07-14 12:47:08 +0530

    Update App.js

--- Day-6/src/App.js
+++ Day-6/src/App.js
@@ -6,10 +6,6 @@ import Home from './components/home';
 import Glam from './components/Glam';
 import About from './components/About';
 import Register from './components/register';
-import Contact from './components/contact';
-import Cart from './components/Cart';
-import Orders from './components/Orders';
-import Products from './components/Products';
 function App() {
   return (
       
@@ -20,14 +16,9 @@ function App() {
       <Routes>
       
       <Route path='/' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
       <Route path='/home' element={<Home/>}/>
       <Route path='/glam' element={<Glam/>}/>
       <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/orders' element={<Orders/>}/>
-      <Route path='/products' element={<Products/>}/>
       <Route path='/register' element={<Register/>}/>
       </Routes>
     </Router>
